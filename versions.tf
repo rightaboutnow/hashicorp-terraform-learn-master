a8ef74d82d1e9a0c3f466c716078de2fb5930d63
Jenson Thomas <jensonzthomas@outloook.com>
2026-06-08 21:47:08 +0800

    added random provider

--- versions.tf
+++ versions.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.76"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.9.0"
+    }
   }
 
   # Remote state in Azure Blob Storage.
